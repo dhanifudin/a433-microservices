c593b48bb860717bdd20e13f3d82680cc3d6ab56
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2023-07-21 10:54:06 +0900

    add build script

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Build the image and set the tag dhanifudin/karsajobs:latest
+# from current directory context
+docker build -t dhanifudin/karsajobs:latest .
+
+# Authenticate into Docker Hub
+echo $PASSWORD_DOCKER_HUB | docker login -u dhanifudin --password-stdin
+
+# Push the image
+docker push dhanifudin/karsajobs:latest
